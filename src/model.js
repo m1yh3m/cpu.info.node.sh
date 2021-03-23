f1a49c201dbc462df870467321e4985ce1cd8234
Anubhav Saini <iamanubhavsaini@gmail.com>
2021-03-23 09:12:26 +0530

    Major version bump. v2022.3.23

--- src/model.js
+++ src/model.js
@@ -1,5 +1,4 @@
-const {cpus, freemem, totalmem} = require('os');
-const {red, green} = require('./colors');
+const {cpuInfo} = require('@f0c1s/cpu.info.node.lib')
 
 function cpusStream(duration, times, cb) {
   let i = 0;
@@ -13,41 +12,6 @@ function cpusStream(duration, times, cb) {
   }, duration);
 }
 
-const areSame = (cpus, key) => cpus
-  .map(c => c[key])
-  .every(x => x === cpus[0][key]);
-
-const areAllModelSame = cpus => areSame(cpus, 'model');
-
-function currentMemoryUsage() {
-  const total = totalmem();
-  const free = freemem(), freepc = Math.floor(free / total * 100);
-  const used = total - free, usedpc = Math.ceil(used / total * 100);
-  return {
-    free, freepc, total, used, usedpc,
-    ks: {free: free / 1024, used: used / 1024, total: total / 1024},
-    ms: {free: free / 1024 / 1024, used: used / 1024 / 1024, total: total / 1024 / 1024},
-    gs: {free: free / 1024 / 1024 / 1024, used: used / 1024 / 1024 / 1024, total: total / 1024 / 1024 / 1024},
-    bar: `${red(Array(usedpc).fill('-').join(''))}${green(Array(freepc).fill('-').join(''))}`
-  };
-}
-
-function cpuInfo() {
-  return cpuInfoModel(cpus());
-}
-
-function cpuInfoModel(data) {
-  const shouldRemoveModel = areAllModelSame(data);
-  const memory = currentMemoryUsage();
-  const cpus = data.map(i => i.times).map(({user, sys, idle}) => ({user, sys, idle}));
-  return {
-    model: shouldRemoveModel ? data[0].model : 'Many model.',
-    memory,
-    cpus
-  };
-}
-
 module.exports = {
-  cpusStream,
-  cpuInfo
+  cpusStream
 };
